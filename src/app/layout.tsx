0f036622fb49069fc0104125c1e90776a1942943
enes yasin gedik <gedikas@hotmail.com>
2024-05-14 14:57:11 +0300

    creating homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import Navbar from '@/components/navbar';
 import Footer from '@/components/footer/Footer';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { useRouter } from 'next/navigation';
-import { GetServerSideProps } from 'next';
 
 config.autoAddCss = false;
 
@@ -19,33 +17,14 @@ export const metadata = {
 
 interface RootLayoutProps {
 	children: React.ReactNode;
-	pathname: string;
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	// URL yolunu context parametresinden alıyoruz
-	const { url } = context.req;
-
-	// Bu bilgiyi props olarak komponente aktarıyoruz
-	return {
-		props: { pathname: url },
-	};
-};
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children, pathname }) => {
-	// '/science/homepage' yolunu kontrol eder
-	const isHomepage = pathname === '/science/homepage';
-
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 	return (
 		<html lang="en">
 			<body className={font.className}>
-				{/* Eğer 'science/homepage' sayfası değilse, section içerisinde children render edilir */}
-				{!isHomepage && (
-					<section className="max-w-screen-xl mx-auto">{children}</section>
-				)}
-				{/* Eğer 'science/homepage' sayfası ise, direkt children render edilir */}
-				{isHomepage && children}
 				<Navbar />
+				{children}
 				<Footer />
 			</body>
 		</html>
--- src/app/science/homepage/page.tsx
+++ src/app/science/homepage/page.tsx
@@ -3,13 +3,31 @@ import React from 'react';
 
 const HomePage = () => {
 	return (
-		<div>
-			<Image
-				src="/images/science/homepage/1.png"
-				alt="Oyun"
-				width={200}
-				height={200}
-			/>
+		<div className="flex w-full h-screen">
+			<div className="relative flex-1 h-full">
+				<Image
+					src="/images/science/homepage/1.png"
+					alt="Oyun"
+					layout="fill" // resmin div'e sığdırılması
+					objectFit="cover"
+				/>
+			</div>
+			<div className="relative flex-1 h-full">
+				<Image
+					src="/images/science/homepage/2.png"
+					alt="Oyun"
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
+			<div className="relative flex-1 h-full">
+				<Image
+					src="/images/science/homepage/3.png"
+					alt="Oyun"
+					layout="fill"
+					objectFit="cover"
+				/>
+			</div>
 		</div>
 	);
 };
